3af5329a450eb21cd047605afad6339fda2f158f
le chung phuong <phuonglecva@gmail.com>
2021-01-18 15:56:47 +0700

    Added bai3.py GET Customer Shopify API

--- crawl/bai3.py
+++ crawl/bai3.py
@@ -6,3 +6,42 @@
 # 	- docs: 
 # https://shopify.dev/tutorials/authenticate-a-private-app-with-shopify-admin#make-authenticated-requests
 # https://shopify.dev/docs/admin-api/rest/reference/customers/customer#index-2020-04
+# import shopify
+# shop_url = "https://phuonglc-store.myshopify.com/admin" % ()
+# site = shopify.ShopifyResource.set_site(shop_url)
+# print(site)
+import shopify
+shopify.ShopifyResource.set_site("https://phuonglc-store.myshopify.com/admin/api/2021-01")
+shopify.ShopifyResource.set_user('2318600785f6cd5c0d5aae2db652f4bd')
+shopify.ShopifyResource.set_password('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')
+
+customers = shopify.Customer.find()
+list_attribute = []
+data = []
+customers_datas = []
+for attr in customers[0].attributes:
+    list_attribute.append(attr)
+for customer in customers:
+    # print(customer.attributes)
+    data.append(customer.attributes)
+    customers_datas.append([customer.attributes[att] for att in customer.attributes])
+
+import csv
+fields = list_attribute
+    
+# data rows of csv file  
+rows = customers_datas
+    
+# name of csv file  
+filename = "customers_shopifies.csv"
+    
+# writing to csv file  
+with open(filename, 'w') as csvfile:  
+    # creating a csv writer object  
+    csvwriter = csv.writer(csvfile)  
+        
+    # writing the fields  
+    csvwriter.writerow(fields)  
+        
+    # writing the data rows  
+    csvwriter.writerows(rows) 
